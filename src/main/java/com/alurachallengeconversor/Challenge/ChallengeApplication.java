90006bbfb60922d57f66a043af0670ca85235ed8
LeonardoRS12 <157629044+LeonardoRS12@users.noreply.github.com>
2024-07-10 20:40:31 -0600

    Se actualizaron los archivos
    Se añadieron los archivos finales del programa.

---
+++ src/main/java/com/alurachallengeconversor/Challenge/ChallengeApplication.java
@@ -0,0 +1,17 @@
+package com.alurachallengeconversor.Challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeApplication.class, args);
+	}
+
+}
